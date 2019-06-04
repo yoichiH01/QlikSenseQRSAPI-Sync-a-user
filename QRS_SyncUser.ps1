84fa03061af7cb3017078ef704eabff9ba5c8439
yoichiH01 <46386743+yoichiH01@users.noreply.github.com>
2019-06-04 10:34:41 +1000

    Add files via upload

---
+++ QRS_SyncUser.ps1
@@ -0,0 +1,38 @@
+﻿
+# MIT License
+# 
+# Copyright (c) 2019 Yoichi Hirotake
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE`
+# SOFTWARE.
+
+$username = "dan2" #please enter username you want to sync
+$userdirectory = "Domain" #please enter userdirectory name 
+$hdrs = @{}
+$hdrs.Add("X-Qlik-Xrfkey","examplexrfkey123")
+$hdrs.Add("X-Qlik-User", "UserDirectory=Domain;UserId=Administrator") #UserDirectory and UserID is the one which you run Qlik Sense Services
+#$hdrs
+$xrfkey="examplexrfkey123"
+#$xrfkey
+$cert = Get-ChildItem -Path "Cert:\CurrentUser\My" | Where {$_.Subject -like '*QlikClient*'}
+#$cert
+$Data = Get-Content C:\ProgramData\Qlik\Sense\Host.cfg
+$FQDN = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($($Data)))
+$FQDN
+$syncuser = Invoke-RestMethod -Uri "https://$($FQDN):4242/qrs/user/sync/$($userdirectory)/$($username)?xrfkey=$($xrfkey)" -Method Get -Headers $hdrs -ContentType 'application/json' -Certificate $cert
+#$syncuser
